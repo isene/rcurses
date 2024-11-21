8dd3bead03cb6f15c4d63e101ccacc31ae58354d
Geir <g@isene.com>
2024-11-21 22:37:38 +0100

    0.8: Bugfixes. Tested and fully functional

--- rcurses_example.rb
+++ rcurses_example.rb
@@ -12,11 +12,15 @@ pane_left.align  = "c"
 pane_right.align = "r"
 
 pane_top.text      = "This is an information line at the top".b.i
-pane_left.text     = "\n\n" + "Here is a pane to be reckoned with".b
+pane_left.text     = "\n\n" + "Here is a pane to be reckoned with".b.u
 pane_right.text    = "\n\n" + "And here is the right pane.  "
 pane_bottom.prompt = "Enter any text and press ENTER: ".b
 
 pane_top.refresh   
 pane_left.refresh  
 pane_right.refresh 
+pane_bottom.editline
+
+pane_bottom.prompt = "You wrote: " + pane_bottom.text.pure.i + " (now hit ENTER again)"
+pane_bottom.text = ""
 pane_bottom.editline
