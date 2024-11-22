c8664b1d2f39b714d14986140a1fe243d2fcd746
Geir <g@isene.com>
2024-11-22 09:46:57 +0100

    0.8.5: Better rcurses_example.rb

--- rcurses_example.rb
+++ rcurses_example.rb
@@ -2,18 +2,17 @@
 
 require 'rcurses'
 
-# pane      = Rcurses::Pane.new(             startx,        starty,           width,            height,  fg,  bg)
-pane_top    = Rcurses::Pane.new(                  1,             1,   Rcurses::MAXw,                 1, 255, 236)
-pane_bottom = Rcurses::Pane.new(                  1, Rcurses::MAXh,   Rcurses::MAXw,                 1, 236, 254)
-pane_left   = Rcurses::Pane.new(                  1,             2, Rcurses::MAXw/2, Rcurses::MAXh - 2,  17, 117)
-pane_right  = Rcurses::Pane.new(Rcurses::MAXw/2 + 1,             2, Rcurses::MAXw/2, Rcurses::MAXh - 2, 255,  52)
+# pane      = Rcurses::Pane.new(             startx,        starty,               width,            height,  fg,  bg)
+pane_top    = Rcurses::Pane.new(                  1,             1,       Rcurses::MAXw,                 1, 255, 236)
+pane_bottom = Rcurses::Pane.new(                  1, Rcurses::MAXh,       Rcurses::MAXw,                 1, 236, 254)
+pane_left   = Rcurses::Pane.new(                  2,             3, Rcurses::MAXw/2 - 2, Rcurses::MAXh - 4,  52, nil)
+pane_right  = Rcurses::Pane.new(Rcurses::MAXw/2 + 1,             2,     Rcurses::MAXw/2, Rcurses::MAXh - 2, 255,  52)
 
-pane_left.align  = "c"
-pane_right.align = "r"
+pane_left.border   = true
 
 pane_top.text      = "This is an information line at the top".b.i
-pane_left.text     = "\n\n" + "Here is a pane to be reckoned with".b.u
-pane_right.text    = "\n\n" + "And here is the right pane.  "
+pane_left.text     = `ls --color`
+pane_right.text    = "Output of lsblk:\n\n" + `lsblk`
 pane_bottom.prompt = "Enter any text and press ENTER: ".b
 
 pane_top.refresh   
@@ -22,5 +21,7 @@ pane_right.refresh
 pane_bottom.editline
 
 pane_bottom.prompt = "You wrote: " + pane_bottom.text.pure.i + " (now hit ENTER again)"
-pane_bottom.text = ""
+pane_right.text   += "\n\n" + pane_bottom.text.pure.i.b + "  "
+pane_right.refresh
+pane_bottom.text   = ""
 pane_bottom.editline
